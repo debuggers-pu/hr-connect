5da3a95ba7914713b0ea1a646a18fac060559938
sandesh1998 <sandeshpoudel144@gmail.com>
2023-06-04 15:57:34 +0545

    feat : user login forget and reset password

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,12 +2,15 @@ const express = require("express");
 const upload = require("../helper/multer");
 
 
-const {registerUser,emailVerify } = require("../controller/userController");
-const {validateRegisterUser} = require("../validators/userValidator");
+const {registerUser,emailVerify, loginUser, forgotPassword, resetPassword } = require("../controller/userController");
+const {validateRegisterUser, validateLoginUser, validateResetPassword} = require("../validators/userValidator");
 
 const userRoute = express.Router();  
 
 userRoute.post("/register", upload.single("avatar"), validateRegisterUser, registerUser);
 userRoute.get("/emailVerify", emailVerify);
+userRoute.post("/login",validateLoginUser, loginUser);
+userRoute.post("/forgot-password", forgotPassword);
+userRoute.post("/reset-password", validateResetPassword, resetPassword);
 
 module.exports = userRoute;
