47da2452c5599c25ad92e19e6400ecd26e73bd7a
sandesh1998 <sandeshpoudel144@gmail.com>
2023-08-26 13:52:11 +0545

    feat : attendance through wifi only

--- controller/attendanceController.js
+++ controller/attendanceController.js
@@ -1,55 +1,75 @@
 const Attendance = require("../model/attendance.js");
+const wifi = require("node-wifi");
 
-// Create attendance
+// Create Attendance
+
 const createAttendance = async (req, res) => {
   try {
-    const { employeeName, attendanceType, date, status, reason } = req.body;
-    const attendance = new Attendance({
-      employeeName,
-      attendanceType,
-      date,
-      status,
-      reason,
+    wifi.init({
+      iface: null,
     });
-    // // create attendance ONLY WITH SPECIFIC WIFI MAC ADDRESS (for now)
-    // if (req.body.macAddress !== "00:00:00:00:00:00") {
-    //   return res.status(400).json({ error: "Invalid MAC Address" });
-    // }
+    wifi.getCurrentConnections((err, currentConnections) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send({
+          message: "Some error occurred while getting wifi connection.",
+        });
+      } else {
+        const collegeWifi = "404 NOT FOUND ERROR__2.4";
+        const connectedToCollegeWifi = currentConnections.find(
+          (connection) => connection.ssid === collegeWifi
+        );
+        if (connectedToCollegeWifi) {
+          const { employeeName, attendanceType, date, status, reason } =
+            req.body;
+          const attendance = new Attendance({
+            employeeName,
+            attendanceType,
+            date,
+            status,
+            reason,
+          });
 
-    //check wifi ip address lies within the range of the college's wifi ip address
-    if (
-      !["192.168.1.119", "192.168.1.120", "192.168.1.121"].includes(
-        req.body.wifiIpAddress
-      )
-    ) {
-      const wifiIpAddress = req.body.wifiIpAddress;
-      const collegeWifiIpAddresses = [
-        "192.168.1.119",
-        "192.168.1.123",
-        "192.168.1.124",
-      ];
-      if (!collegeWifiIpAddresses.includes(wifiIpAddress)) {
-        return res.status(400).json({ error: "Invalid Wifi IP Address" });
+          Attendance.findOne({
+            employeeName: employeeName,
+            date: date,
+          })
+            .then((existingAttendance) => {
+              if (existingAttendance) {
+                return res.status(400).json({
+                  error: "Attendance already exists for the specified date",
+                });
+              }
+              // Save Attendance in the database
+              attendance
+                .save()
+                .then((data) => {
+                  res.send({
+                    message: "Attendance created successfully",
+                    data,
+                  });
+                })
+                .catch((err) => {
+                  res.status(500).send({
+                    message:
+                      err.message ||
+                      "Some error occurred while making the Attendance.",
+                  });
+                });
+            })
+            .catch((err) => {
+              res.status(500).send({
+                message:
+                  err.message ||
+                  "Some error occurred while checking existing Attendance.",
+              });
+            });
+        }
       }
-    }
-
-    const existingAttendance = await Attendance.findOne({
-      employeeName: employeeName,
-      date: date,
     });
-    if (existingAttendance) {
-      return res
-        .status(400)
-        .json({ error: "Attendance already exists for the specified date" });
-    }
-
-    const newAttendance = await attendance.save();
-    res
-      .status(200)
-      .json({ message: "Attendance created successfully", newAttendance });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: "Error creating attendance" });
+    return res.status(500).json({ error: "Attendance failed!" });
   }
 };
 
