535f2efc8b9642148314199a80e7aaeddd5831f6
sandesh1998 <sandeshpoudel144@gmail.com>
2023-08-28 08:40:35 +0545

    feat : get all attendance

--- routes/attendanceRoute.js
+++ routes/attendanceRoute.js
@@ -3,6 +3,7 @@ const express = require("express");
 const {
   createAttendance,
   clockOutAttendance,
+  getAllAttendance,
 } = require("../controller/attendanceController");
 
 const { isLoggedIn } = require("../middleware/auth");
@@ -11,5 +12,6 @@ const attendanceRoute = express.Router();
 
 attendanceRoute.post("/createAttendance", isLoggedIn, createAttendance);
 attendanceRoute.patch("/clockOutAttendance", isLoggedIn, clockOutAttendance);
+attendanceRoute.get("/getAllAttendance", isLoggedIn, getAllAttendance);
 
 module.exports = attendanceRoute;
